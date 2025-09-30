5ed195eff71f6a66653a848587a6eb719dd8af47
AndreyEA <enonesohc@mail.ru>
2025-09-30 12:20:32 +0300

    7.4 Практикуемся в реализации HTTP API клиентов Практика реализации HTTP API клиентов: /api/v1/operations

---
+++ clients/http/gateway/operations/client.py
@@ -0,0 +1,172 @@
+from typing import TypedDict
+from httpx import Response, QueryParams
+from clients.http.client import HTTPClient
+
+
+class GetOperationQueryDict(TypedDict):
+    """
+    Структура данных для получения списка операций пользователя
+    """
+    accountId: str
+
+
+class MakeOperationQueryDict(TypedDict):
+    """
+    Структура данных для отправления запроса на платёжные операции
+    """
+    status: str
+    amount: int
+    cardId: str
+    accountId: str
+
+
+class MakePurchaseOperationQueryDict(TypedDict):
+    """
+    Структура данных для отправления запроса на операцию покупки
+    """
+    status: str
+    amount: int
+    cardId: str
+    accountId: str
+    category: str
+
+
+class OperationsGatewayHTTPClient(HTTPClient):
+    """
+    Клиент для взаимодействия с /api/v1/operations сервиса http-gateway.
+    """
+
+    def get_operation_api(self, operation_id: str) -> Response:
+        """
+        Выполняет GET-запрос на получение информации об операции по operation_id
+
+        :param operation_id: Строка с operation_id, например '123'.
+        :return: Объект httpx.Response с данными об операции с operation_id
+        """
+        return self.get(f"/api/v1/operations/{operation_id}")
+
+    def get_operation_receipt_api(self, operation_id: str) -> Response:
+        """
+        Выполняет GET-запрос на получение чека по операции по operation_id
+
+        :param operation_id: Строка с operation_id, например '123'.
+        :return: Объект httpx.Response с данными о чеке по операции с operation_id
+        """
+        return self.get(f"/api/v1/operation-receipt/{operation_id}")
+
+    def get_operations_api(self, query: GetOperationQueryDict) -> Response:
+        """
+        Выполняет GET-запрос на получение списка операций для определённого счёта
+
+        :param query: Словарь с параметрами запроса, например: {'userId': '123'}.
+        :return: Объект httpx.Response с данными списка операций для определённого счёта
+        """
+        return self.get("/api/v1/operations/operations", params=QueryParams(**query))
+
+    def get_operations_summary_api(self, query: GetOperationQueryDict) -> Response:
+        """
+        Выполняет GET-запрос на получение статистики по операциям для определённого счёта
+
+        :param query: Словарь с параметрами запроса, например: {'userId': '123'}.
+        :return: Объект httpx.Response с данными об операции.
+        """
+        return self.get("/api/v1/operations/operations-summary", params=QueryParams(**query))
+
+    def make_fee_operation_api(self, query: MakeOperationQueryDict) -> Response:
+        """
+        Выполняет POST-запрос на создание операции комиссии
+
+        :param query: Словарь с параметрами запроса, например: {
+                                                                  "status": "FAILED",
+                                                                  "amount": 0,
+                                                                  "cardId": "string",
+                                                                  "accountId": "string"
+                                                                }.
+        :return: Объект httpx.Response с данными об операции.
+        """
+        return self.post("/api/v1/operations/make-fee-operation", json=QueryParams(**query))
+
+    def make_top_up_operation_api(self, query: MakeOperationQueryDict) -> Response:
+        """
+        Выполняет POST-запрос на создание операции пополнения
+
+        :param query: Словарь с параметрами запроса, например: {
+                                                                  "status": "FAILED",
+                                                                  "amount": 0,
+                                                                  "cardId": "string",
+                                                                  "accountId": "string"
+                                                                }.
+        :return: Объект httpx.Response с данными об операции.
+        """
+        return self.post("/api/v1/operations/make-top-up-operation", json=QueryParams(**query))
+
+    def make_cashback_operation_api(self, query: MakeOperationQueryDict) -> Response:
+        """
+        Выполняет POST-запрос на создание операции кэшбэка
+
+        :param query: Словарь с параметрами запроса, например: {
+                                                                  "status": "FAILED",
+                                                                  "amount": 0,
+                                                                  "cardId": "string",
+                                                                  "accountId": "string"
+                                                                }.
+        :return: Объект httpx.Response с данными об операции.
+        """
+        return self.post("/api/v1/operations/make-cashback-operation", json=QueryParams(**query))
+
+    def make_transfer_operation_api(self, query: MakeOperationQueryDict) -> Response:
+        """
+        Выполняет POST-запрос на создание операции перевода
+
+        :param query: Словарь с параметрами запроса, например: {
+                                                                  "status": "FAILED",
+                                                                  "amount": 0,
+                                                                  "cardId": "string",
+                                                                  "accountId": "string"
+                                                                }.
+        :return: Объект httpx.Response с данными об операции.
+        """
+        return self.post("/api/v1/operations/make-transfer-operation", json=QueryParams(**query))
+
+    def make_purchase_operation_api(self, query: MakePurchaseOperationQueryDict) -> Response:
+        """
+        Выполняет POST-запрос на создание операции покупки
+
+        :param query: Словарь с параметрами запроса, например: {
+                                                                  "status": "FAILED",
+                                                                  "amount": 0,
+                                                                  "cardId": "string",
+                                                                  "accountId": "string",
+                                                                  "category": "string"
+                                                                }
+        :return: Объект httpx.Response с данными об операции.
+        """
+        return self.post("/api/v1/operations/make-purchase-operation", json=QueryParams(**query))
+
+    def make_bill_payment_operation_api(self, query: MakeOperationQueryDict) -> Response:
+        """
+        Выполняет POST-запрос на создание операции оплаты по счету
+
+        :param query: Словарь с параметрами запроса, например: {
+                                                                  "status": "FAILED",
+                                                                  "amount": 0,
+                                                                  "cardId": "string",
+                                                                  "accountId": "string"
+                                                                }.
+        :return: Объект httpx.Response с данными об операции.
+        """
+        return self.post("/api/v1/operations/make-bill-payment-operation", json=QueryParams(**query))
+
+    def make_cash_withdrawal_operation_api(self, query: MakeOperationQueryDict) -> Response:
+        """
+        Выполняет POST-запрос на создание операции снятия наличных денег
+
+        :param query: Словарь с параметрами запроса, например: {
+                                                                  "status": "FAILED",
+                                                                  "amount": 0,
+                                                                  "cardId": "string",
+                                                                  "accountId": "string"
+                                                                }.
+        :return: Объект httpx.Response с данными об операции.
+        """
+        return self.post("/api/v1/operations/make-cash-withdrawal-operation", json=QueryParams(**query))
