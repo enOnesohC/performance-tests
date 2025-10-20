e12a755225c14978d4f0d8caf8550a62f8f907c9
AndreyEA <enonesohc@mail.ru>
2025-10-20 12:29:48 +0300

    11.3 Практика генерации данных с помощью сидингов Практика: реализация сидинга для сценария выпуска новой виртуальной карты

---
+++ seeds/scenarios/existing_user_issue_virtual_card.py
@@ -0,0 +1,38 @@
+from seeds.scenario import SeedsScenario
+from seeds.schema.plan import SeedsPlan, SeedUsersPlan, SeedAccountsPlan
+
+
+class ExistingUserIssueVirtualCardSeedsScenario(SeedsScenario):
+    """
+    Сценарий сидинга для существующего пользователя, который выполняет операцию покупки.
+    Создаёт 300 пользователей, каждому открывает один дебетовый счёт
+    """
+
+    @property
+    def plan(self) -> SeedsPlan:
+        """
+        План сидинга, который описывает, сколько пользователей нужно создать
+        и какие именно данные для них генерировать.
+        В данном случае создаём 300 пользователей, каждому открываем один дебетовый счёт
+        """
+        return SeedsPlan(
+            users=SeedUsersPlan(
+                count=300,  # Количество пользователей
+                deposit_accounts=SeedAccountsPlan(
+                    count=1  # Количество счётов на пользователя
+                )
+            ),
+        )
+
+    @property
+    def scenario(self) -> str:
+        """
+        Название сценария сидинга, которое будет использоваться для сохранения данных.
+        """
+        return "existing_user_issue_virtual_card"
+
+
+if __name__ == "__main__":
+    # Если файл запускается напрямую, создаём объект сценария и запускаем его.
+    seeds_scenario = ExistingUserIssueVirtualCardSeedsScenario()
+    seeds_scenario.build()
